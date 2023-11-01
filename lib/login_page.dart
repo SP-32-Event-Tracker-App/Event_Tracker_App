b92d5e761f192d5f94068562fa88192bcfd1ef65
Renwarren <warren.chendjou@gmail.com>
2023-11-01 15:14:44 -0400

    Complete the login page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -9,104 +9,152 @@ class _LoginPageState extends State<LoginPage> {
   TextEditingController _usernameController = TextEditingController();
   TextEditingController _passwordController = TextEditingController();
   bool _obscureText = true;
+  bool _rememberMe = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   title: Text('Login Page'),
-      // ),
       body: Padding(
         padding: const EdgeInsets.all(15.0),
         child: Container(
           margin: const EdgeInsets.symmetric(vertical: 30),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Image.asset(
-                'assets/images/KSU-logo.jpg',
-                height: 280,
-              ),
-              SizedBox(
-                height: 16,
-              ),
-              TextField(
-                controller: _usernameController,
-                decoration: InputDecoration(
-                    labelText: 'abc@email.com',
-                    prefixIcon: Icon(Icons.email),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10))),
-              ),
-              const SizedBox(height: 16),
-              TextField(
-                controller: _passwordController,
-                obscureText: _obscureText,
-                decoration: InputDecoration(
-                    labelText: 'Your password',
-                    prefixIcon: Icon(Icons.lock),
-                    suffixIcon: IconButton(
-                      icon: Icon(_obscureText
-                          ? Icons.visibility
-                          : Icons.visibility_off),
-                      onPressed: () {
-                        setState(() {
-                          _obscureText = !_obscureText;
-                        });
-                      },
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Image.asset(
+                  'assets/images/KSU-logo.jpg',
+                  height: 260,
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                TextField(
+                  controller: _usernameController,
+                  decoration: InputDecoration(
+                      labelText: 'abc@email.com',
+                      prefixIcon: Icon(Icons.email),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10))),
+                ),
+                const SizedBox(height: 16),
+                TextField(
+                  controller: _passwordController,
+                  obscureText: _obscureText,
+                  decoration: InputDecoration(
+                      labelText: 'Your password',
+                      prefixIcon: Icon(Icons.lock),
+                      suffixIcon: IconButton(
+                        icon: Icon(_obscureText
+                            ? Icons.visibility
+                            : Icons.visibility_off),
+                        onPressed: () {
+                          setState(() {
+                            _obscureText = !_obscureText;
+                          });
+                        },
+                      ),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10))),
+                ),
+                SizedBox(height: 16),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      elevation: 3, minimumSize: Size(double.infinity, 50)),
+                  onPressed: () {
+                    _login();
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "SIGN IN",
+                        style: TextStyle(fontSize: 18),
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(
+                        width: 8,
+                      ),
+                      Icon(Icons.arrow_forward),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Switch(
+                            value: _rememberMe,
+                            activeColor: Colors.white,
+                            activeTrackColor: Colors.deepPurple,
+                            inactiveTrackColor: Colors.white,
+                            splashRadius: 50.0,
+                            onChanged: (value) =>
+                                setState(() => _rememberMe = value)),
+                        Text(
+                          "Remember Me",
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ],
                     ),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10))),
-              ),
-              SizedBox(height: 16),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    elevation: 3, minimumSize: Size(double.infinity, 50)),
-                onPressed: () {
-                  _login();
-                },
-                child: Row(
+                    GestureDetector(
+                      onTap: () {
+                        // Handle "Forgot Password" action
+                        print("Forgot Password");
+                      },
+                      child: Text(
+                        "Forgot Password?",
+                        style: TextStyle(
+                          color: Colors.blue,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 24),
+                Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Text(
-                      "SIGN IN",
-                      style: TextStyle(fontSize: 18),
-                      textAlign: TextAlign.center,
-                    ),
-                    SizedBox(
-                      width: 8,
-                    ),
-                    Icon(Icons.arrow_forward),
+                      "OR",
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey.shade800,
+                      ),
+                    )
                   ],
                 ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      Checkbox(
-                          value: false,
-                          onChanged: (value) => print("Remember Me")),
-                      Text("Remember Me"),
-                    ],
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      // Handle "Forgot Password" action
-                      print("Forgot Password");
-                    },
-                    child: Text(
-                      "Forgot Password?",
+                SizedBox(height: 18),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Don't have an account? ",
                       style: TextStyle(
-                        color: Colors.blue,
-                        decoration: TextDecoration.underline,
+                        fontSize: 20,
+                        color: Colors.black87,
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ],
+                    GestureDetector(
+                      onTap: () {
+                        print("Sign Up");
+                      },
+                      child: Text(
+                        "Sign up",
+                        style: TextStyle(
+                          color: Colors.blue,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
