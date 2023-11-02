c34e2fd64bdfb0d9e75bd73be19894131326a4f7
Renwarren <warren.chendjou@gmail.com>
2023-11-01 21:36:53 -0400

    Create the home page screen

--- lib/widgets/home_page.dart
+++ lib/widgets/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:event_tracker_app/widgets/event_card.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,10 +12,99 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Text("HEYYYYYYYYYYYYYYYYYYY"),
-        ],
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(80.0),
+        child: AppBar(
+          backgroundColor: Colors.pink.shade200,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(20.0),
+              bottomRight: Radius.circular(20.0),
+            ),
+          ),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              IconButton(
+                icon: const Icon(
+                  Icons.menu,
+                  color: Colors.white,
+                ),
+                onPressed: () {
+                  // Add your search functionality here
+                },
+              ),
+              const Column(
+                children: [
+                  Text(
+                    "Current Location",
+                    style: TextStyle(
+                      color: Colors.white38,
+                      fontSize: 16,
+                    ),
+                  ),
+                  Text(
+                    "Kennesaw, GA",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                    ),
+                  )
+                ],
+              ),
+              IconButton(
+                icon: const Icon(
+                  Icons.notifications,
+                  color: Colors.white,
+                ),
+                onPressed: () {
+                  // Add more options or menu functionality here
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: Container(
+        // color: Colors.black,
+        margin: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  "Upcoming Events",
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {},
+                  child: Text(
+                    "See all events ➡️",
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.grey.shade700,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  EventCard(),
+                  EventCard(),
+                  EventCard(),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
